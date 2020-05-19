d945defa0cb43c6a326d825f40d7b82587efa18a
Sergey Ostanin <ostanioni@gmail.com>
2020-05-20 02:21:48 +0700

    v 0.0.2

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './app.css';
 function hello(msg: string): void {
   console.log(msg);
 }
-let msg: string = "hello, Serega";
-hello(msg);
+let msg: string = 'hello, Serega';
+let A = 'jhjk';
+hello(msg + A);
 // let el:ParentNode = document.createElement('div');
