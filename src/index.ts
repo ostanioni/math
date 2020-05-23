985c6e5c28e0524f53b9a8b268c785cc6ec476f2
Sergey Ostanin <ostanioni@gmail.com>
2020-05-24 01:54:14 +0700

    v 0.0.2

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,7 @@ function hello(msg: string): void {
 let msg: string = 'hello, Serega';
 let A = 'jhjk';
 hello(msg + A);
+console.log('after breakpoint...');
 // let el:ParentNode = document.createElement('div');
+let AD: string = 'dwedcw';
+AD = 1;
