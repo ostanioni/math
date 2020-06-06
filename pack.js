b1bbfd258bc098f50b0cf409eb462c62cd253fa2
Sergey Ostanin <ostanioni@gmail.com>
2020-06-06 22:03:06 +0700

    v 0.0.1

--- pack.js
+++ pack.js
@@ -1,34 +1,33 @@
-const {exec} = require('child_process');
+const { exec } = require('child_process');
 
 // WEBPACK
-exec ('npm i -D webpack webpack-bundle-analyzer webpack-cli webpack-dev-server webpack-merge',
-(error, stdout, stderr) => {
+exec(
+  'npm i -D webpack webpack-bundle-analyzer webpack-cli webpack-dev-server webpack-merge',
+  (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
     }
     console.log(`stdout: ${stdout}`);
     console.error(`stderr: ${stderr}`);
-  });
+  },
+);
 // HTML
-exec ('npm i -D html-loader html-webpack-plugin',
-(error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+exec('npm i -D html-loader html-webpack-plugin', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
 // CSS
 // TS
-exec ('npm i -D typescript',
-(error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
-
+exec('npm i -D typescript', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
