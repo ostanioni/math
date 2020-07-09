a2e469d680620711e58ae25653d74998bf8d251c
Sergey Ostanin <ostanioni@gmail.com>
2020-07-09 18:02:13 +0700

    v 0.0.3

---
+++ src/formats/formats.js
@@ -0,0 +1,23 @@
+const formats = {
+  "A4": {
+    "width": 210,
+    "height": 297
+  },
+  "A3": {
+    "width": 297,
+    "height": 420
+  },
+  "A2": {
+    "width": 420,
+    "height": 594
+  },
+  "A1": {
+    "width": 594,
+    "height": 841
+  },
+  "A0": {
+    "width": 841,
+    "height": 1189
+  }
+}
+export default formats;
